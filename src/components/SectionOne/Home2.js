db4629d8227032b09386dc6a15a62ee6a52b1747
spr887011 <spr887011@gmail.com>
2023-08-24 17:15:58 +0530

    Added the image and set it is visible for mobile view

--- src/components/SectionOne/Home2.js
+++ src/components/SectionOne/Home2.js
@@ -2,13 +2,16 @@ import React from "react";
 import './sectionOne.css';
 import {Col, Row} from "react-bootstrap"; // Importing the custom CSS
 import image from "./questions.png";
+import imageMobile from "./question women.png";
 function Home2() {
     return (
         <div className="outer-container">
             <div className="inner-container">
                 <Row className="center-contents">
                     <Col md={6} xs="auto">
-                        <img src={image} className="ques-img"
+                        <img src={image} className="ques-img desktop"
+                        alt = "confused"/>
+                         <img src={imageMobile} className="ques-img mobile"
                         alt = "confused"/>
                     </Col>
                     <Col md={6} xs={"auto"}>
