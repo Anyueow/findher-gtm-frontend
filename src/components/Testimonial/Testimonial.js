2480a17cd27ad3b53661a67fc67e9710118a320d
spr887011 <spr887011@gmail.com>
2023-08-25 13:27:13 +0530

    Design changes for Testimonial and added Carousel buttons

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,7 +1,7 @@
-import React, { useState , useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import image1 from "./images/17.png";
 import image2 from "./images/18.png";
 import image3 from "./images/19.png";
@@ -9,176 +9,202 @@ import image4 from "./images/20.png";
 import image5 from "./images/21.png";
 import "./testimonial.css";
 
-
 const testimonials = [
-    {
-        name: "Sneha M.",
+  {
+    name: "Sneha M.",
 
-        message: "Findher has been a game-changer in my job search. It gave me insights I didn't "
-                 + "even know I needed and connected me with a job that's a perfect fit. Highly recommend!",
-        image: image1,
-    },
-    {
-        name: "Radhika S.",
-        message: "As a working mom, finding the right job seemed overwhelming. But Findher's "
-                 + "personalized matches and support made the process seamless. I'm so grateful!",
-        image: image2,
-    },
-    {
-        name: "Pooja K.",
+    message:
+      "Findher has been a game-changer in my job search. It gave me insights I didn't " +
+      "even know I needed and connected me with a job that's a perfect fit. Highly recommend!",
+    image: image1,
+  },
+  {
+    name: "Radhika S.",
+    message:
+      "As a working mom, finding the right job seemed overwhelming. But Findher's " +
+      "personalized matches and support made the process seamless. I'm so grateful!",
+    image: image2,
+  },
+  {
+    name: "Pooja K.",
 
-        message: "I was searching for a platform that truly understood the challenges women like me "
-                 + "face in their careers. Their focus on my specific goals made all the difference",
-        image: image3,
-    },
-    {
-        name: "Radhika S.",
-        message: "As a working mom, finding the right job seemed overwhelming. But Findher's "
-                 + "personalized matches and support made the process seamless. I'm so grateful!",
-        image: image4,
-    },
-    {
-        name: "Pooja KA.",
+    message:
+      "I was searching for a platform that truly understood the challenges women like me " +
+      "face in their careers. Their focus on my specific goals made all the difference",
+    image: image3,
+  },
+  {
+    name: "Radhika S.",
+    message:
+      "As a working mom, finding the right job seemed overwhelming. But Findher's " +
+      "personalized matches and support made the process seamless. I'm so grateful!",
+    image: image4,
+  },
+  {
+    name: "Pooja KA.",
 
-        message: "I was searching for a platform that truly understood the challenges women like me "
-                 + "face in their careers. Their focus on my specific goals made all the difference",
-        image: image5,
-    },
-    // Add more testimonials as needed
+    message:
+      "I was searching for a platform that truly understood the challenges women like me " +
+      "face in their careers. Their focus on my specific goals made all the difference",
+    image: image5,
+  },
+  // Add more testimonials as needed
 ];
 
 function TestimonialSection() {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [lastIndex, setLastIndex] = useState(testimonials.length-1);
 
-    // Adjust items per page based on window width
-    const [itemsPerPage, setItemsPerPage] = useState(3);
+  // Adjust items per page based on window width
+  const [itemsPerPage, setItemsPerPage] = useState(3);
 
-    const [isDragging, setIsDragging] = useState(false);
-    const [dragStartX, setDragStartX] = useState(0);
-    const [dragStartY, setDragStartY] = useState(0);
-    const containerRef = useRef(null);
-    const [touchEndX, setTouchEndX] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragStartX, setDragStartX] = useState(0);
+  const [dragStartY, setDragStartY] = useState(0);
+  const containerRef = useRef(null);
+  const [touchEndX, setTouchEndX] = useState(0);
 
-    const handleTouchStart = (e) => {
-        setIsDragging(true);
-        setDragStartX(e.touches[0].clientX);
-        setDragStartY(e.touches[0].clientY);
-      };
-    
-      const handleTouchMove = (e) => {
-        if (!isDragging) return;
+  const handleTouchStart = (e) => {
+    setIsDragging(true);
+    setDragStartX(e.touches[0].clientX);
+    setDragStartY(e.touches[0].clientY);
+  };
 
-        // Ignore touch move if there are multiple touches
-        if (e.touches.length !== 1 || Math.abs(e.touches[0].clientY - dragStartY) > 50) {
-          setIsDragging(false);
-          return;
-        }
+  const handleTouchMove = (e) => {
+    if (!isDragging) return;
 
-        const dragDistance = e.touches[0].clientX - dragStartX;
-        const threshold = 150; // Adjust this threshold as needed
+    // Ignore touch move if there are multiple touches
+    if (
+      e.touches.length !== 1 ||
+      Math.abs(e.touches[0].clientY - dragStartY) > 50
+    ) {
+      setIsDragging(false);
+      return;
+    }
 
-        if (dragDistance > threshold) {
-          // Swipe from left to right (swipe right)
-          setTouchEndX(e.touches[0].clientX); // Set the touch end position
-        } else if (dragDistance < -threshold) {
-          // Swipe from right to left (swipe left)
-          setTouchEndX(e.touches[0].clientX); // Set the touch end position
-        }
-      };
-    
-      const handleTouchEnd = (e) => {
-        if (!isDragging) return;
-        setIsDragging(false);
+    const dragDistance = e.touches[0].clientX - dragStartX;
+    const threshold = 150; // Adjust this threshold as needed
 
-        const dragDistance = dragStartX - touchEndX; // Calculate the total drag distance
-        const threshold = 150; // Adjust this threshold as needed
-      
-        if (dragDistance > threshold) {
-          // Swipe from right to left (swipe left)
-          handleNext();
-        } else if (dragDistance < -threshold) {
-          // Swipe from left to right (swipe right)
-          handlePrev();
-        }
-      
-        // Clear touch end position
-        setTouchEndX(0);
-      };
+    if (dragDistance > threshold) {
+      // Swipe from left to right (swipe right)
+      setTouchEndX(e.touches[0].clientX); // Set the touch end position
+    } else if (dragDistance < -threshold) {
+      // Swipe from right to left (swipe left)
+      setTouchEndX(e.touches[0].clientX); // Set the touch end position
+    }
+  };
 
-    useEffect(()=>{
-        const calculateItemsPerPage = () => {
-            return window.innerWidth <= 780 ? 1 : 3;
-        };
-        
-        // Set the initial value of itemsPerPage
-        setItemsPerPage(calculateItemsPerPage());
+  const handleTouchEnd = (e) => {
+    if (!isDragging) return;
+    setIsDragging(false);
 
-        // Update itemsPerPage when the window width changes
-        const handleResize = () => {
-            setItemsPerPage(calculateItemsPerPage());
-        };
-       
-        window.addEventListener('resize', handleResize);
-    },[])
+    const dragDistance = dragStartX - touchEndX; // Calculate the total drag distance
+    const threshold = 150; // Adjust this threshold as needed
 
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => Math.min(prevIndex + itemsPerPage, testimonials.length - itemsPerPage));
+    if (dragDistance > threshold) {
+      // Swipe from right to left (swipe left)
+      handleNext();
+    } else if (dragDistance < -threshold) {
+      // Swipe from left to right (swipe right)
+      handlePrev();
+    }
+
+    // Clear touch end position
+    setTouchEndX(0);
+  };
+
+  useEffect(() => {
+    const calculateItemsPerPage = () => {
+      return window.innerWidth <= 780 ? 1 : 3;
     };
 
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) => Math.max(prevIndex - itemsPerPage, 0));
+    // Set the initial value of itemsPerPage
+    setItemsPerPage(calculateItemsPerPage());
+
+    // Update itemsPerPage when the window width changes
+    const handleResize = () => {
+      setItemsPerPage(calculateItemsPerPage());
     };
 
-    const visibleTestimonials = testimonials.slice(currentIndex, currentIndex + itemsPerPage);
+    window.addEventListener("resize", handleResize);
+  }, []);
 
-    return (
-      <section>
-        <Container className=" jus center-contents">
-          <div>
-            <h1 className="test-head">
-              {" "}
-              What these women have to say about{" "}
-              <span className="headspan"> FindHer</span> :
-            </h1>
-          </div>
-        </Container>
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      Math.min(prevIndex + itemsPerPage, testimonials.length - itemsPerPage)
+    );
+  };
 
-        <Container className="mar">
-          <button className="arrow-button desktop" onClick={handlePrev}>
-            <AiOutlineLeft className="opalescent-arrow" />
-          </button>
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - itemsPerPage, 0));
+  };
 
-          <Row
-            className="center-contents"
-            onTouchStart={handleTouchStart}
-            onTouchMove={handleTouchMove}
-            onTouchEnd={handleTouchEnd}
-            ref={containerRef}
-          >
-            {visibleTestimonials.map((testimonial) => (
-              <Col md={3} className="testimonial center-contents">
-                <Row>
+  const visibleTestimonials = testimonials.slice(
+    currentIndex,
+    currentIndex + itemsPerPage
+  );
+  return (
+    <section>
+      <Container className=" jus center-contents">
+        <div>
+          <h1 className="test-head">
+            {" "}
+            What these women have to say about{" "}
+            <span className="headspan"> FindHer</span> :
+          </h1>
+        </div>
+      </Container>
+
+      <Container className="mar">
+        <button className="arrow-button desktop" onClick={handlePrev}>
+          <AiOutlineLeft className="opalescent-arrow" />
+        </button>
+        <Row
+          className="center-contents"
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleTouchEnd}
+          ref={containerRef}
+        >
+          {visibleTestimonials.map((testimonial) => (
+            <Col md={3} className="testimonial center-contents">
+              <Row>
+                <Col
+                  md={7}
+                  xs={7}
+                  className="d-flex align-items-center justify-content-center"
+                >
+                  <h1
+                    className="header"
+                    style={{ fontSize: "20px", fontWeight: "550" }}
+                  >
+                    {testimonial.name}
+                  </h1>
+                </Col>
+                <Col md={5} xs={5}>
                   <img
                     className="card-img"
                     src={testimonial.image}
                     alt="profile"
                   />
-                  <h1 className="header">{testimonial.name}</h1>
-
-                  <h1 className="Quotes">“</h1>
-                  <p className="cardSub">{testimonial.message}</p>
-                  <h1 className="Quotes"> ”</h1>
-                </Row>
-              </Col>
-            ))}
-          </Row>
-          <button className="arrow-button desktop" onClick={handleNext}>
-            <AiOutlineRight className="opalescent-arrow" />
-          </button>
-        </Container>
-        {/* ... other components or sections ... */}
-      </section>
-    );
+                </Col>
+                <p className="cardSub mt-3">{testimonial.message}</p>
+                <div className="mobile dot-testimonial-div">
+                  <div className={`dot-testimonial mx-2 ${currentIndex ===0? "active":""}`}></div>
+                  <div className={`dot-testimonial mx-2 ${currentIndex >= 1? (currentIndex < lastIndex? "active":""):""}`}></div>
+                  <div className={`dot-testimonial mx-2 ${currentIndex ===lastIndex? "active":""}`}></div>
+                </div>
+              </Row>
+            </Col>
+          ))}
+        </Row>
+        <button className="arrow-button desktop" onClick={handleNext}>
+          <AiOutlineRight className="opalescent-arrow" />
+        </button>
+      </Container>
+      {/* ... other components or sections ... */}
+    </section>
+  );
 }
 
 export default TestimonialSection;
