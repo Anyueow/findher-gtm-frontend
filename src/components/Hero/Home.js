d695a6211590e168c60139d6aa8ade950e007a2d
anyueow <anyushah@gmail.com>
2023-08-30 12:57:40 +0530

    fixed home changes Wp45

--- src/components/Hero/Home.js
+++ src/components/Hero/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 
 
 import base from "./Images/hero base.png";
-import mobbase from "./Images/hero women.png";
+import mobbase from "./Images/hero women cut.png";
 
 
 
@@ -22,18 +22,19 @@ function Home() {
                     <Col md={7} xs={12} className="hero-text">
                         <div className="text-content">
                     <h1 className="main-name">
-                        Insights on workplaces in <span className="text-deco">India</span>,
+                        Insights on Workplaces in <span className="text-deco">India</span>,
                         <span className="connector"> for </span>
                         <span className="highlight">women </span>
                         <span className="connector">by</span>
                         <span className="highlight-alt"> women.</span>
                     </h1>
-                    <h3 className="description hero-description">
+                    <h3 className="description
+                     hero-description">
                         Learn what it’s like to work at a company from the women who have been there.
                     </h3>
                             <a href="https://www.surveymonkey.com/r/NMD3GRV"
                                className="signup-btn desktop"
-                               style={{marginTop:"5%", display: 'inline-block', textDecoration: 'none'}}
+                               style={{marginTop:"3%", display: 'inline-block', textDecoration: 'none'}}
                                target="_blank"
                                rel="noopener noreferrer">
                                 Sign up today!
@@ -45,7 +46,7 @@ function Home() {
                     <Col md={5} xs={12}>
                 <div>
                     <img src={base} className="base-image desktop" alt="Findher Demo"/>
-                    <img src={mobbase} className="base-image mobile" alt="Findher Demo"/>
+                    <img src={mobbase} className="mob-img mobile" alt="Findher Demo"/>
                     <div className="d-flex justify-content-center">
                     <a href="https://www.surveymonkey.com/r/NMD3GRV"
                                className="signup-btn mobile"
