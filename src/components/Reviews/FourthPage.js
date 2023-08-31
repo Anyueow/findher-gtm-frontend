3c227fe67ff840336de404cb8b223d21dd952c8d
spr887011 <spr887011@gmail.com>
2023-08-31 22:21:58 +0530

    Reviews/FourthPage.js, review_three validation completed

--- src/components/Reviews/FourthPage.js
+++ src/components/Reviews/FourthPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, Container, Row, Col, InputGroup } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col, InputGroup, Toast } from 'react-bootstrap';
 import { FaStar } from 'react-icons/fa';
 import "./reviewStyles.css";
 import { useNavigate } from "react-router-dom";
@@ -17,6 +17,11 @@ const FourthPage = () => {
     const [diversityRating, setDiversityRating] = useState(0);
     const [safetyRating, setSafetyRating] = useState(0);
     const [compensationRating, setCompensationRating] = useState(0);
+    const [isFormValid, setisFormValid] = useState({
+        message: "",
+        for: "",
+        status: false,
+      });
 
     const createRatingInputGroup = (name, setter) => {
         return (
@@ -31,7 +36,6 @@ const FourthPage = () => {
                                 value={ratingValue}
                                 onClick={() => setter(ratingValue)}
                                 className="star-radio"
-                                required
                             />
                             <FaStar
                                 className="star-icon review-four-star-icon"
@@ -52,10 +56,65 @@ const FourthPage = () => {
 
     const navigate = useNavigate();  // Import useHistory for navigation
    
+    function checkForm() {
+      if (flexibilityRating <= 0) {
+        setisFormValid({
+          message: "Please slect flexibility rating star",
+          for: "flexibility",
+          status: true,
+        });
+        return true;
+      }
+      else if (managementRating <= 0) {
+        setisFormValid({
+          message: "Please slect management rating star",
+          for: "management",
+          status: true,
+        });
+        return true;
+      }
+      else if (coWorkersRating  <= 0) {
+        setisFormValid({
+          message: "Please slect co-workers rating star",
+          for: "co-workers",
+          status: true,
+        });
+        return true;
+      }
+      else if (diversityRating  <= 0) {
+        setisFormValid({
+          message: "Please slect diversity rating star",
+          for: "diversity",
+          status: true,
+        });
+        return true;
+      }
+      else if (safetyRating  <= 0) {
+        setisFormValid({
+          message: "Please slect safety rating star",
+          for: "safety",
+          status: true,
+        });
+        return true;
+      }
+      else if (compensationRating  <= 0) {
+        setisFormValid({
+          message: "Please slect compensation rating star",
+          for: "compensation",
+          status: true,
+        });
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
 
     const handleSubmit = async (e) => {
-
         e.preventDefault();
+        if(checkForm()){
+            return;
+        }
         const submitRatings = async () => {
             const token = localStorage.getItem("token");
 
@@ -166,6 +225,23 @@ const FourthPage = () => {
                     </Row>
                 </Row>
             </Form>
+            {isFormValid.status && (
+          <Toast className="Second-page-validation-toast">
+            <Toast.Body>
+              <p >{isFormValid.message}</p>
+              <Button
+                type="button"
+                className="btn"
+                onClick={() =>
+                  setisFormValid((prevState) => ({
+                    ...prevState,
+                    status: false,
+                  }))
+                }
+              >Ok</Button>
+            </Toast.Body>
+          </Toast>
+        )}
         </Container>
         </div>
     );
