218c352adbe856b4e0dacfe3bbe7b551a7c14b35
anyueow <anyushah@gmail.com>
2023-09-04 10:28:29 +0530

    added new design

---
+++ src/finalDesign/SectionTwo/SectionTwo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './SectionTwo.css';
+import {Col, Row} from "react-bootstrap";
+
+
+function SectionOne() {
+    return (
+        <div className="job-search-section">
+            <Row className="justify-content-center">
+                <Col className="justify-content-center">
+                    <h2>Why Choose FindHer
+                    </h2>
+                    <h1>FindHer is your partner in career success.
+                        We help you find the job of your dreams.</h1>
+
+                </Col>
+            </Row>
+
+        </div>
+    );
+}
+
+export default SectionOne;
