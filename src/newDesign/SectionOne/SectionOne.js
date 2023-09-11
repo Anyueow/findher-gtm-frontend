ec863116176beece91cc2680c18562c5d46bd24b
spr887011 <spr887011@gmail.com>
2023-09-11 20:07:05 +0530

    resolving build error

--- src/newDesign/SectionOne/SectionOne.js
+++ src/newDesign/SectionOne/SectionOne.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState,useRef } from "react";
+import React from "react";
 import "./SectionOne.css";
 import { Button, Col, Row } from "react-bootstrap";
 import demo from "./Group 167.webp";
