b2341ce58497fb4177621baedadac77ae131735a
anyueow <anyushah@gmail.com>
2023-10-28 18:26:43 +0530

    adde dnew post API and caught error. Caused by messages being passed to handleSubmit (e & action variable)
    -> For furture. Create two different handlesubmit functions for the two buttons

--- src/LandingPage/Forms/businessforms.js
+++ src/LandingPage/Forms/businessforms.js
@@ -12,64 +12,129 @@ const PopupForm = ({ isVisible, onClose }) => {
         const { name, value } = e.target;
         setFormData(prev => ({ ...prev, [name]: value }));
     };
+    //
+    // const handleSubmit = async (e, action) => {
+    //     e.preventDefault();
+    //
+    //     const { email, companyName, name } = formData;
+    //
+    //     if (!email || !companyName || !name) {
+    //         toast.error("Please fill out all the fields", {
+    //             position: toast.POSITION.TOP_CENTER,
+    //         });
+    //         return;
+    //     }
+    //
+    //     try {
+    //         const response = await fetch(process.env.REACT_APP_URL + "business/joinnow", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 "X-CSRF-Token": csrfToken,
+    //             },
+    //             credentials: "include",
+    //             body: JSON.stringify({
+    //                                      email: formData.email,
+    //                                      companyName: formData.companyName,
+    //                                      name: formData.name,
+    //                                  }),
+    //         });
+    //
+    //         if (response.ok) {
+    //             const message = action === 'info'
+    //                             ? "We just sent you an email, check your inbox for details :)"
+    //                             : "Redirecting you to schedule a call...";
+    //             toast.success(message, {
+    //                 position: toast.POSITION.TOP_RIGHT,
+    //             });
+    //
+    //             if (action === 'schedule') {
+    //                 window.open('https://calendly.com/asurana/chat-with-anjali?month=2023-10',
+    //                             '_blank');
+    //             }
+    //         } else {
+    //             // Assuming the server responds with a JSON payload in case of an error
+    //             const serverResponse = await response.json();
+    //
+    //             // Constructing an error message based on server response or a default message
+    //             const errorMessage = serverResponse.message || 'Server responded with an error';
+    //             throw new Error(errorMessage);
+    //         }
+    //     }   catch (error) {
+    //     toast.error("Oops that didn't work!", {
+    //         position: toast.POSITION.TOP_RIGHT,
+    //     });
+    //     console.error('Submission error:', error);
+    // }
+    //
+    //
+    // onClose(); // Close the modal
+    // };
+    const handleSubmit = async (action) => {
+        console.log(action);  // Check what is being received
 
-    const handleSubmit = async (e, action) => {
-        e.preventDefault();
-
-        const { email, companyName, name } = formData;
-
-        if (!email || !companyName || !name) {
+        // Validate form data
+        const { name, companyName, email } = formData;
+        if (!name || !companyName || !email) {
             toast.error("Please fill out all the fields", {
                 position: toast.POSITION.TOP_CENTER,
             });
             return;
         }
 
+        // Prepare form data for submission
+        const submissionData = {
+            name,
+            companyName,
+            email
+        };
+
         try {
-            const response = await fetch(process.env.REACT_APP_URL + "business/joinnow", {
-                method: "POST",
+            // Send the form data to the server-side route that handles email sending
+            const response = await fetch('/send-alert-email', {
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",
+                    'Content-Type': 'application/json',
                     "X-CSRF-Token": csrfToken,
-                },
-                credentials: "include",
-                body: JSON.stringify({
-                                         email: formData.email,
-                                         companyName: formData.companyName,
-                                         name: formData.name,
-                                     }),
+                                },
+                body: JSON.stringify(submissionData),
             });
 
             if (response.ok) {
-                const message = action === 'info'
-                                ? "We just sent you an email, check your inbox for details :)"
-                                : "Redirecting you to schedule a call...";
-                toast.success(message, {
-                    position: toast.POSITION.TOP_RIGHT,
-                });
 
-                if (action === 'schedule') {
+                if(action === 'info') {
+                    // Handle success scenario here
+                    const message = "We just sent you an email, check your inbox for details :)";
+                    toast.success(message, {
+                        position: toast.POSITION.TOP_RIGHT,
+                    });
+                }
+
+               else {
                     window.open('https://calendly.com/asurana/chat-with-anjali?month=2023-10',
                                 '_blank');
+                    const message = "Redirecting you to schedule a call...";
+                    toast.success(message, {
+                        position: toast.POSITION.TOP_RIGHT,
+                    });
                 }
             } else {
-                // Assuming the server responds with a JSON payload in case of an error
-                const serverResponse = await response.json();
-
-                // Constructing an error message based on server response or a default message
-                const errorMessage = serverResponse.message || 'Server responded with an error';
-                throw new Error(errorMessage);
+                // If the server responded with a status code outside the range of 2xx, throw an error
+                const errorData = await response.json(); // Assuming server responds with JSON
+                throw new Error(errorData.message || 'Error occurred while sending email.');
             }
-        }   catch (error) {
-        toast.error("Oops that didn't work!", {
-            position: toast.POSITION.TOP_RIGHT,
-        });
-        console.error('Submission error:', error);
-    }
+        } catch (error) {
+            // Handle errors in sending the form data or email
+            toast.error("Oops, we couldn't send your form. Please try again.", {
+                position: toast.POSITION.TOP_RIGHT,
+            });
+            console.error('Error:', error.message);
+        }
 
-
-    onClose(); // Close the modal
+        // Close the modal (if applicable)
+        onClose();
     };
+
 
     return (
         <Modal show={isVisible} onHide={onClose} centered>
@@ -113,7 +178,7 @@ const PopupForm = ({ isVisible, onClose }) => {
                             <Button
                                 variant="primary"
                                 className="buttclass"
-                                onClick={(e) => handleSubmit(e, 'info')}
+                                onClick={() => handleSubmit('info')}
                             >
                                 Get More Info
                             </Button>
@@ -122,7 +187,7 @@ const PopupForm = ({ isVisible, onClose }) => {
                             <Button
                                 variant="secondary"
                                 className="buttclass"
-                                onClick={(e) => handleSubmit(e, 'schedule')}
+                                onClick={() => handleSubmit('schedule')}
                             >
                                 Schedule a Call
                             </Button>
